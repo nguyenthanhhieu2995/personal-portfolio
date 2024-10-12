6ced5e529bed47a917616a3bc069cac75d49ac6d
Hieu Nguyen <nguyenthanhhieu2995@gmail.com>
2024-10-12 13:26:55 +0700

    build header UI

---
+++ src/components/Header.tsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import Navbar from "./Navbar";
+
+export default function Header() {
+  const [navOpen, setNavOpen] = useState(false);
+  return (
+    <header className="fixed top-0 left-0 right-0 z-40 w-full h-20 flex items-center bg-gradient-to-b from-zinc-900 to-zinc-900/0">
+      <div className="max-w-screen-xl w-full mx-auto px-4 flex items-center justify-between md:px-6 md:grid md:grid-cols-[1fr,3fr,1fr]">
+        <h1>
+          <a href="/" className="logo">
+            <img
+              src="/images/logo.svg"
+              alt="Hieu Nguyen"
+              className="w-10 h-10"
+            />
+          </a>
+        </h1>
+        <div
+          className="relative md:justify-self-center"
+        >
+          <button className="menu-btn md:hidden" onClick={() => setNavOpen((navOpen) => !navOpen)}>
+            <span className="material-symbols-rounded">
+              {navOpen ? "close" : "menu"}
+            </span>
+          </button>
+          <Navbar navOpen={navOpen} />
+        </div>
+        <a
+          href="#contact"
+          className="btn btn-secondary max-md:hidden md:justify-self-end"
+        >
+          Contact Me
+        </a>
+      </div>
+    </header>
+  );
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,82 @@
+import { useRef, useEffect } from "react";
+
+interface NavbarProps {
+  navOpen: boolean;
+}
+
+export default function Navbar({ navOpen }: NavbarProps) {
+    const lastActiveLink = useRef<HTMLElement | null>(null);
+  const activeBox = useRef<HTMLDivElement>(null);
+  
+  const initActiveBox = () => {
+    console.log(lastActiveLink.current);
+    console.log(activeBox.current);
+    activeBox.current!.style.top = lastActiveLink.current!.offsetTop + "px";
+    activeBox.current!.style.left = lastActiveLink.current!.offsetLeft + "px";
+    activeBox.current!.style.width = lastActiveLink.current!.offsetWidth + "px";
+    activeBox.current!.style.height =
+      lastActiveLink.current!.offsetHeight + "px";
+      
+  };
+  useEffect(initActiveBox, []);
+  
+  window.addEventListener("resize", initActiveBox);
+  const activeCurrentLink = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    const target = event.currentTarget;
+    lastActiveLink.current?.classList.remove("active");
+    target.classList.add("active");
+    lastActiveLink.current = target;
+
+    const { offsetTop, offsetLeft, offsetWidth, offsetHeight } = target;
+    const boxStyle = activeBox.current!.style;
+
+    boxStyle.top = `${offsetTop}px`;
+    boxStyle.left = `${offsetLeft}px`;
+    boxStyle.width = `${offsetWidth}px`;
+    boxStyle.height = `${offsetHeight}px`;
+  };
+  const navItems = [
+    {
+      label: "Home",
+      link: "#home",
+      className: "nav-link active",
+      ref: lastActiveLink,
+    },
+    {
+      label: "About",
+      link: "#about",
+      className: "nav-link",
+    },
+    {
+      label: "Work",
+      link: "#work",
+      className: "nav-link",
+    },
+    {
+      label: "Reviews",
+      link: "#reviews",
+      className: "nav-link",
+    },
+    {
+      label: "Contact",
+      link: "#contact",
+      className: "nav-link md:hidden",
+    },
+  ];
+  return (
+    <nav className={`navbar ${navOpen ? "active" : ""}`}>
+      {navItems.map(({ label, link, className, ref }, key) => (
+        <a
+          key={key}
+          href={link}
+          ref={ref as React.LegacyRef<HTMLAnchorElement>}
+          className={className}
+          onClick={activeCurrentLink}
+        >
+          {label}
+        </a>
+      ))}
+      <div className="active-box" ref={activeBox}></div>
+    </nav>
+  );
+}
